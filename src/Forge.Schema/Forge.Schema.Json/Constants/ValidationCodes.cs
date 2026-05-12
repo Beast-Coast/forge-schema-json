68d601fb55e7557818dadb93f4f027ea1d94e345
Nick Cicchetti <nc_cicchetti@yahoo.com>
2026-05-12 17:16:34 -0400

    added sln and constants

---
+++ src/Forge.Schema/Forge.Schema.Json/Constants/ValidationCodes.cs
@@ -0,0 +1,11 @@
+namespace DefaultNamespace;
+
+public static class ValidationCodes
+{
+    public const string FileTooLarge = "FileTooLarge";
+    public const string InvalidUtf8 = "InvalidUtf8";
+    public const string SchemaViolation =  "SchemaViolation";
+    public const string SuspiciousContent =  "SuspiciousContent";
+    public const string DeserializationFailed =  "DeserializationFailed";
+    public const string SemanticViolation = "SemanticViolation";
+}
